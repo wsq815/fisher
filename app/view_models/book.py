7af03a0cdd18084befa221326e41ca4f72368adf
wuson <wsq815@foxmail.com>
2019-12-18 21:16:33 +0800

    course7-2

--- app/view_models/book.py
+++ app/view_models/book.py
@@ -3,4 +3,32 @@
 # @Author : wuson
 # @File : book.py
 
-print(11)
+class BookViewModel:
+    @classmethod
+    def package_single(cls, data, keyword):
+        returned = {
+            'books': [],
+            'total': 0,
+            'keyword': keyword
+        }
+        if data:
+            returned['total'] = 1
+            returned['books'] = [cls.__cut_book_data(data)]
+        return returned
+
+    @classmethod
+    def package_collection(cls, data, keyword):
+        pass
+
+    @classmethod
+    def __cut_book_data(cls, data):
+        book = {
+            'title': data['title'],
+            'publisher': data['publisher'],
+            'pages': data['pages'],
+            'author': '、'.join(data['author']),
+            'price': data['price'],
+            'summary': data['summary'],
+            'image': data['image'],
+        }
+        return book
